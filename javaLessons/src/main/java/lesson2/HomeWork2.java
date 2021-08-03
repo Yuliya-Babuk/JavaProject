b0c7dec0daada4a4d6e18cc74e90e2166ca25a39
Yuliya-Babuk <yuliya.babuk@eveoncontainers.com>
2021-08-03 16:29:07 +0300

    Lesson_2. Add new methods: 1. isYearLeap 2. isLeftPartEqualsToRightPart 3. Print min and max elements of array

--- javaLessons/src/main/java/lesson2/HomeWork2.java
+++ javaLessons/src/main/java/lesson2/HomeWork2.java
@@ -1,26 +1,31 @@
 
 package lesson2;
 
-import java.util.Arrays;
-import java.util.OptionalInt;
-
 public class HomeWork2 {
     public static void main(String[] args) {
         System.out.println(within10and20(3, 15));
         isPositiveOrNegative(0);
         System.out.println(isNegative(-5));
         printWordNTimes("Nice day", 5);
+        System.out.println(isYearLeap(2020));
         reverseElementsInArray(new int[]{0, 1, 1, 0, 1});
         fillArray();
+        System.out.println(isLeftPartEqualsToRightPart(new int[]{1, 2, 5, 8}));
         multiplyBy2IfLessThen6(new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1});
         fillDiagonal();
-        int[] array = createAndInitializeArray(4, 5);
+        createAndInitializeArray(5);
         //offsetArray(new int[]{0, 1, 2, 3, 4}, 3);
     }
 
-    private static int[] createAndInitializeArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
+    private static boolean isYearLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0)
+            return true;
+        return year % 400 == 0;
+    }
+
+    private static int[] createAndInitializeArray(int initialValue) {
+        int[] array = new int[4];
+        for (int i = 0; i < 4; i++) {
             array[i] = initialValue;
             System.out.print(array[i] + " ");
         }
@@ -55,7 +60,24 @@ public class HomeWork2 {
             arr[i] = i + 1;
             System.out.print(arr[i] + " ");
         }
-        System.out.println();
+        System.out.println("\nMin element is " + Math.min(arr[0], arr[99]));
+        System.out.println("Max element is " + Math.max(arr[0], arr[99]));
+    }
+
+    public static boolean isLeftPartEqualsToRightPart(int[] arr) {
+        int sumLeftPart = 0;
+        int sumRightPart = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            sumLeftPart += arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                sumRightPart += arr[j];
+            }
+            if (sumLeftPart == sumRightPart) {
+                return true;
+            }
+            sumRightPart =0;
+        }
+        return false;
     }
 
     private static void printWordNTimes(String text, int times) {
