94fa3e421cebb14588ae81d9a213327cb84aa689
Yuliya-Babuk <yuliya.babuk@eveoncontainers.com>
2021-08-15 14:22:18 +0300

    Lesson_5. Add implementation of task #1. Minor changes in the code

--- javaLessons/src/main/java/lesson5/HomeWork5.java
+++ javaLessons/src/main/java/lesson5/HomeWork5.java
@@ -5,18 +5,37 @@ import java.util.Arrays;
 
 public class HomeWork5 {
     public static void main(String[] args) {
+
+        String[] arrStrings = new String[]{"Apple", "Orange"};
+        System.out.println(Arrays.deepToString(arrStrings));
+        swap(arrStrings, 0, 1);
+        System.out.println(Arrays.deepToString(arrStrings));
+
         Apple apple1 = new Apple();
         Apple apple2 = new Apple();
         Orange orange1 = new Orange();
         Orange orange2 = new Orange();
         Orange orange3 = new Orange();
-        Box<Apple> boxWithApples = new Box<>(apple1, apple2);
-        Box<Orange> boxWithOranges = new Box<>(orange1, orange2, orange3);
-        Box<Apple> boxWithApplesNew = new Box<>();
-        System.out.println("Box of " + boxWithApples.getFruits().size() + " apples weights " + boxWithApples.getBoxWeight());
+        ArrayList<Apple> fruitsApp = new ArrayList<>();
+        fruitsApp.add(apple1);
+        fruitsApp.add(apple2);
+        Box<Apple> boxWithApples = new Box<>(fruitsApp);
+        ArrayList<Orange> fruitsOr = new ArrayList<>();
+        fruitsOr.add(orange1);
+        fruitsOr.add(orange2);
+        fruitsOr.add(orange3);
+        Box<Orange> boxWithOranges = new Box<>(fruitsOr);
+               System.out.println("Box of " + boxWithApples.getFruits().size() + " apples weights " + boxWithApples.getBoxWeight());
         System.out.println("Box of " + boxWithOranges.getFruits().size() + " oranges weights " + boxWithOranges.getBoxWeight());
         System.out.println("Are these boxes are equal? - " + boxWithApples.compare(boxWithOranges));
+        Box<Apple> boxWithApplesNew = new Box<>(new ArrayList<>());
         boxWithApples.moveTo(boxWithApplesNew);
+    }
+
+    public static void swap(Object[] arr, int indexFirst, int indexSecond) {
+        var temp = arr[indexFirst];
+        arr[indexFirst] = arr[indexSecond];
+        arr[indexSecond] = temp;
     }
 }
 
@@ -59,12 +78,12 @@ class Box<T extends Fruit> {
         this.fruits = fruits;
     }
 
-    public Box(T... fruits) {
-        this.fruits = new ArrayList<>(Arrays.asList(fruits));
+    public Box(ArrayList<T> fruits) {
+        this.fruits = fruits;
     }
 
-    public void add(T... fruits) {
-        this.fruits.addAll(Arrays.asList(fruits));
+    public void addFruits(ArrayList<T> fruits) {
+        this.fruits.addAll(fruits);
     }
 
     public float getBoxWeight() {
@@ -80,7 +99,7 @@ class Box<T extends Fruit> {
     }
 
     public void moveTo(Box newBox) {
-        newBox.getFruits().addAll(this.getFruits());
+        newBox.addFruits(this.getFruits());
         clear();
     }
 }
